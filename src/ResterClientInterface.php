d70d202ffd07d88d5a37ce001dddbd8832fcdf3d
MilesChou <jangconan@gmail.com>
2018-02-10 14:58:47 +0800

    Modify main interface

--- src/ResterClientInterface.php
+++ src/ResterClientInterface.php
@@ -2,38 +2,14 @@
 
 namespace Corp104\Rester;
 
-use Psr\Http\Message\ResponseInterface;
-
 interface ResterClientInterface
 {
     /**
-     * @param string $url
-     * @param array $queryParams
-     * @return ResponseInterface
-     */
-    public function get(string $url, array $queryParams = []): ResponseInterface;
-
-    /**
-     * @param string $url
+     * @param $apiName
+     * @param array $binding
      * @param array $parsedBody
      * @param array $queryParams
-     * @return ResponseInterface
+     * @return mixed
      */
-    public function post(string $url, array $parsedBody = [], array $queryParams = []): ResponseInterface;
-
-    /**
-     * @param string $url
-     * @param array $parsedBody
-     * @param array $queryParams
-     * @return ResponseInterface
-     */
-    public function put(string $url, array $parsedBody = [], array $queryParams = []): ResponseInterface;
-
-    /**
-     * @param string $url
-     * @param array $parsedBody
-     * @param array $queryParams
-     * @return ResponseInterface
-     */
-    public function delete(string $url, array $parsedBody = [], array $queryParams = []): ResponseInterface;
+    public function call($apiName, array $binding = [], array $parsedBody = [], array $queryParams = []);
 }
