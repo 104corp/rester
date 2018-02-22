b31faa1645d4408e8765efd7d2a7eecea10b4989
MilesChou <jangconan@gmail.com>
2018-02-22 16:08:53 +0800

    Add ResterMix class

---
+++ src/ResterMix.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Corp104\Rester;
+
+use Corp104\Rester\Support\CollectionAwareTrait;
+use Corp104\Rester\Plugins\CollectionTrait;
+use Corp104\Rester\Support\CollectionAwareInterface;
+
+/**
+ * Rester Client + Rester Collection class
+ */
+class ResterMix extends ResterClient implements CollectionAwareInterface
+{
+    use CollectionTrait;
+}
